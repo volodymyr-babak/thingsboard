789701edda900261432b2c44309fd6377138b912
Volodymyr Babak <volodymyr.babak@gmail.com>
2023-08-16 15:42:23 +0300

    Added test to cover JSON attr case

--- application/src/test/java/org/thingsboard/server/edge/BaseDeviceEdgeTest.java
+++ application/src/test/java/org/thingsboard/server/edge/BaseDeviceEdgeTest.java
@@ -378,8 +378,10 @@ abstract public class BaseDeviceEdgeTest extends AbstractEdgeTest {
                 "inactivityTimeout", "3600000");
         sendAttributesRequestAndVerify(device, DataConstants.SHARED_SCOPE, "{\"key2\":\"value2\"}",
                 "key2", "value2");
+        sendAttributesRequestAndVerify(device, DataConstants.SERVER_SCOPE, "{\"jsonKey\":{\"nestedJsonKey\":\"nestedJsonValue\"}}",
+                "jsonKey", "{\"nestedJsonKey\":\"nestedJsonValue\"}");
 
-        doDelete("/api/plugins/telemetry/DEVICE/" + device.getUuidId() + "/" + DataConstants.SERVER_SCOPE, "keys", "key1, inactivityTimeout");
+        doDelete("/api/plugins/telemetry/DEVICE/" + device.getUuidId() + "/" + DataConstants.SERVER_SCOPE, "keys", "key1, inactivityTimeout, jsonKey");
         doDelete("/api/plugins/telemetry/DEVICE/" + device.getUuidId() + "/" + DataConstants.SHARED_SCOPE, "keys", "key2");
     }
 
@@ -643,6 +645,9 @@ abstract public class BaseDeviceEdgeTest extends AbstractEdgeTest {
                         break;
                     case LONG_V:
                         Assert.assertEquals(Long.parseLong(expectedValue), keyValueProto.getLongV());
+                        break;
+                    case JSON_V:
+                        Assert.assertEquals(JacksonUtil.toJsonNode(expectedValue), JacksonUtil.toJsonNode(keyValueProto.getJsonV()));
                         break;
                     default:
                         Assert.fail("Unexpected data type: " + keyValueProto.getType());
